101ab7717c2fed6fa9e75cd213cb12a208f5dc4c
NathanRomike <nathanromike@gmail.com>
2016-01-08 10:53:32 -0800

    Add test for multiple words.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -18,10 +18,17 @@ public class AppTest {
   }
 
   @Test
-  public void puzzledWord_shouldReplacedApplewithDashPPLDash_dashppldash() {
+  public void  puzzledWord_shouldReplacedApplewithDashPPLDash_dashppldash() {
     App app = new App();
     String puzzledWord = "-ppl-";
     assertEquals("-ppl-", App.puzzledWord("apple"));
+  }
+
+  @Test
+  public void puzzledWord_shouldReplaceVowelsWithDashes_hdashlldashspacewdashrld() {
+    App app = new App();
+    String puzzledWord = "H-ll- W-rld";
+    assertEquals("H-ll- W-rld", App.puzzledWord("Hello World"));
   }
 
 }
