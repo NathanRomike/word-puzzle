cab7a29e517e40ced0272abff0b232109074d98b
NathanRomike <nathanromike@gmail.com>
2016-01-08 10:10:12 -0800

    Add behavior test for input e to fail RED.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,8 +5,15 @@ public class AppTest {
 
   @Test
   public void puzzledWord_shouldReplaceAWithDash_dash() {
-    App App = new App();
+    App app = new App();
     String puzzledWord = "-";
     assertEquals("-", App.puzzledWord("a"));
+  }
+
+  @Test
+  public void puzzledWord_shouldReplaceEWithDash_dash() {
+    App app = new App();
+    String puzzledWord = "-";
+    assertEquals("-", App.puzzledWord("e"));
   }
 }
